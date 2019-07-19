22f76eaa7f6437ae30da34bf80d72cfa7295cfa7
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-19 13:56:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array){
+  return array.slice(0,2);
+}
+const returnLastTwoDrivers = function(array){
+  return array.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+  
+  const fareMultiplier = function (no){
+    return no * number;
+  }
+  return fareMultiplier;
+}
+
+const fareDoubler = function() {
+  const value = createFareMultiplier(2);
+  return value;
+}
